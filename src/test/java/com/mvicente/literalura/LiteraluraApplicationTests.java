5870f97e8b6d506e2ca0b5a8fc153184a75d2a0b
Manoel Vicente <manoelj.vicente@gmail.com>
2025-01-16 20:25:53 +0000

    first commit

---
+++ src/test/java/com/mvicente/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvicente.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
