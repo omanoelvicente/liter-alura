29da822955ae38141cff3c0e5d6d772600ab4958
Manoel Vicente <manoelj.vicente@gmail.com>
2025-01-17 15:42:25 +0000

    feat: implement validation for HTTP response

--- src/main/java/com/mvicente/literalura/service/ApiConsumer.java
+++ src/main/java/com/mvicente/literalura/service/ApiConsumer.java
@@ -7,17 +7,22 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ApiConsumer {
-    public String requestData(String url) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
+    public String requestData(String urlApi) {
+        HttpClient client =HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
                 .build();
-        HttpResponse<String> response = null;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlApi))
+                .build();
+        HttpResponse<String> response;
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Erro ao consumir API: " + response.statusCode());
+            }
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao enviar requisição: " + e.getMessage(), e);
         }
         String json = response.body();
         return json;
--- src/main/java/com/mvicente/literalura/service/JsonConverter.java
+++ src/main/java/com/mvicente/literalura/service/JsonConverter.java
@@ -2,9 +2,6 @@ package com.mvicente.literalura.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
-
-import java.util.List;
 
 public class JsonConverter implements IJsonConverter{
     private final ObjectMapper mapper = new ObjectMapper();
