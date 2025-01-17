6c76b3b9fa5a0c025c9360fc41f1a90e6dc03b47
Manoel Vicente <manoelj.vicente@gmail.com>
2025-01-17 15:37:16 +0000

    feat: implement JSON deserialization using Jackson ObjectMapper

--- src/main/java/com/mvicente/literalura/service/JsonConverter.java
+++ src/main/java/com/mvicente/literalura/service/JsonConverter.java
@@ -1,4 +1,21 @@
 package com.mvicente.literalura.service;
 
-public class JsonConverter {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
+
+public class JsonConverter implements IJsonConverter{
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T getData(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
