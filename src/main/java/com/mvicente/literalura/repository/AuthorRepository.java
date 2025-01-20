3058d6ee80098799fdb81a8209c29aad2d0b53bb
Manoel Vicente <manoelj.vicente@gmail.com>
2025-01-20 18:41:12 +0000

    feat: Implement rules for data magenement

--- src/main/java/com/mvicente/literalura/repository/AuthorRepository.java
+++ src/main/java/com/mvicente/literalura/repository/AuthorRepository.java
@@ -1,4 +1,14 @@
 package com.mvicente.literalura.repository;
 
-public interface AuthorRepository {
+import com.mvicente.literalura.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByName(String name);
+
+    List<Author> findByBirthYearLessThanEqualAndDeathYearGreaterThan(int year, int year1);
 }
+
