938617212dfd3af8d821883a9c321e9ba0c5793e
Manoel Vicente <manoelj.vicente@gmail.com>
2025-01-20 18:50:22 +0000

    feat: Implement main application class

--- src/main/java/com/mvicente/literalura/LiteraluraApplication.java
+++ src/main/java/com/mvicente/literalura/LiteraluraApplication.java
@@ -1,13 +1,28 @@
 package com.mvicente.literalura;
 
+import com.mvicente.literalura.mainmenu.MainMenu;
+import com.mvicente.literalura.repository.AuthorRepository;
+import com.mvicente.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+	@Autowired
+	private BookRepository bookRepository ;
+
+	@Autowired
+	private AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		MainMenu mainMenu = new MainMenu(bookRepository, authorRepository);
+		mainMenu.showMenu();
+	}
 }
